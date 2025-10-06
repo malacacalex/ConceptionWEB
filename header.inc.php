8e4e0f3f124a04d4b71fe867986c516d0f84e727
malacacalex <malacaxel@gmail.com>
2025-10-06 10:24:48 +0200

    update

--- header.inc.php
+++ header.inc.php
@@ -4,8 +4,8 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?php echo $titre; ?></title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link href="style.css" rel="stylesheet">  
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  </head>    <link href="style.css" rel="stylesheet">  
   </head>
   <body>
   
--- index.php
+++ index.php
@@ -5,7 +5,27 @@
   include('menu.inc.php');
   include('message.inc.php')
 ?>
-  <h1>Hello, world!</h1>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <h1>Projet démenagement &#129503 &#129484</h1>
+    <div>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet rutrum justo, non lobortis neque scelerisque at. Sed cursus et ante mattis tristique. Nam dignissim in risus eu scelerisque. Nam non luctus purus. Phasellus fringilla eros vitae blandit pulvinar. Morbi volutpat dictum iaculis. Mauris et maximus arcu. Cras vehicula dolor dignissim dolor tempus consequat.</p>
+        <p>Praesent quis sapien metus. Fusce eget aliquam mi, egestas auctor erat. Aenean leo nunc, elementum ornare augue vitae, efficitur ultricies mi. Integer id pretium sapien. Ut sagittis est vel leo vehicula mollis. Nulla facilisi. Nullam eget sodales lectus.</p>
+    </div>
+
+    <div class="maDiv">
+        <a href="house1.php"><img src="images/house1.jpg"></a>
+        <img src="images/house1.jpg" class ="col-sm">
+        <img src="images/house1.jpg" class ="col-sm">
+        <img src="images/house1.jpg" class ="col-sm">
+    </div>
+    <br>
+    <div class="maDiv">
+        <img src="images/house1.jpg" class ="col-sm">
+        <img src="images/house1.jpg" class ="col-sm">
+        <img src="images/house1.jpg" class ="col-sm">
+        <img src="images/house1.jpg" class ="col-sm">
+    </div>
+
 <?php
   include('footer.inc.php');
 ?>
