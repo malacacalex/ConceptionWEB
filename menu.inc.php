7cb7df1518b3f7f72934efc3b5e943102d46ee3e
malacacalex <malacaxel@gmail.com>
2025-10-06 11:23:32 +0200

    modif background + menu

--- menu.inc.php
+++ menu.inc.php
@@ -2,7 +2,7 @@
   <div class="container-fluid">
 
     <!-- Partie gauche de la barre -->
-    <a class="navbar-brand" href="index.php">Esigelec</a>
+    <a class="navbar-brand" href="index.php" style="color:white">Esigelec</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -10,23 +10,23 @@
       
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link" aria-current="page" href="index.php">Accueil</a>
+          <a class="nav-link" aria-current="page" href="index.php" style="color:white">Accueil</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="page.php">Page</a>
+          <a class="nav-link" href="page.php" style="color:white">Page</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="autre.php">Autre page</a>
+          <a class="nav-link" href="autre.php" style="color:white">Autre page</a>
         </li>
       </ul>
 
       <!-- Partie droite -->
       <ul class="navbar-nav">
           <li class="nav-item">
-            <a class="nav-link" aria-current="page" href="inscription.php">Inscription</a>
+            <a class="nav-link" aria-current="page" href="inscription.php" style="color:white">Inscription</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="connexion.php">Connexion</a>
+            <a class="nav-link" href="connexion.php" style="color:white">Connexion</a>
           </li>
       </ul>
     </div>
