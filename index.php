d7c45966bb73f5f6d7d625d1a39d98844a162fb6
malacacalex <malacaxel@gmail.com>
2025-10-20 13:46:24 +0200

    juste un test

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
     <h1>Projet démenagement &#129503 &#129484</h1>
     <div>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin aliquet rutrum justo, non lobortis neque scelerisque at. Sed cursus et ante mattis tristique. Nam dignissim in risus eu scelerisque. Nam non luctus purus. Phasellus fringilla eros vitae blandit pulvinar. Morbi volutpat dictum iaculis. Mauris et maximus arcu. Cras vehicula dolor dignissim dolor tempus consequat.</p>
+        <p>Lorem ipsum samet dolor sit amet, consectetur adipiscing elit. Proin aliquet rutrum justo, non lobortis neque scelerisque at. Sed cursus et ante mattis tristique. Nam dignissim in risus eu scelerisque. Nam non luctus purus. Phasellus fringilla eros vitae blandit pulvinar. Morbi volutpat dictum iaculis. Mauris et maximus arcu. Cras vehicula dolor dignissim dolor tempus consequat.</p>
         <p>Praesent quis sapien metus. Fusce eget aliquam mi, egestas auctor erat. Aenean leo nunc, elementum ornare augue vitae, efficitur ultricies mi. Integer id pretium sapien. Ut sagittis est vel leo vehicula mollis. Nulla facilisi. Nullam eget sodales lectus.</p>
     </div>
 
